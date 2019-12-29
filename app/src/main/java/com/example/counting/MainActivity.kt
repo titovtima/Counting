8f0f00453fe18b30862db489d8cb8d8c8be6e5ac
titovtima <titov.tima.spb@yandex.ru>
2019-12-29 14:14:07 +0300

    refactor MainActivity

--- app/src/main/java/com/example/counting/MainActivity.kt
+++ app/src/main/java/com/example/counting/MainActivity.kt
@@ -1,12 +1,9 @@
 package com.example.counting
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -17,65 +14,43 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    val dbHelper = DBHelper(this)
+    private val dbHelper = DBHelper(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val db = dbHelper.writableDatabase
+        loadDBdata()
+        setButtonsListeners()
+        setNubmerTasks()
+        setLevelsList()
+    }
 
-        val cur = db.rawQuery("SELECT id FROM users;", null)
-        if (cur.moveToFirst())
-            do {
-                Status.existID.add(cur.getInt(cur.getColumnIndex("id")))
-            } while (cur.moveToNext())
-        cur.close()
+    private fun update(){
+        userbutton.text = Status.userName
+    }
 
-        userbutton.setOnClickListener {
-            val intent = Intent(this, UsersActivity::class.java)
+    override fun onResume() {
+        super.onResume()
 
-            startActivity(intent)
-        }
+        update()
+    }
 
-        settings.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
+    override fun onStop() {
+        super.onStop()
 
-            startActivity(intent)
-        }
+        try { Status.need = number_tasks.text.toString().toInt() } catch (e : Exception){}
 
-        start.setOnClickListener {
-            try {
-                Status.need = task.text.toString().toInt()
-                val intent = Intent(this, Task::class.java)
-//                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
-                startActivity(intent)
-            } catch (e : Exception){
-                Toast.makeText(this, "Введите количество заданий", Toast.LENGTH_SHORT).show()
-            }
-        }
+        val editor = Status.saveSettings!!.edit()
+        editor.putInt(Status.saveLevelKey, Status.level)
+        editor.putInt(Status.saveNeedKey, Status.need)
+        editor.apply()
+    }
 
+    private fun setNubmerTasks() {
+        number_tasks.setText(Status.need.toString())
 
-        if (!Status.loaded) try {
-            Status.saveSettings = getSharedPreferences(Status.nameFileSettings, Context.MODE_PRIVATE)
-
-            if (Status.saveSettings!!.contains(Status.saveIdKey))
-                Status.userID = Status.saveSettings!!.getInt(Status.saveIdKey, -1)
-            if (Status.saveSettings!!.contains(Status.saveNameKey))
-                Status.userName = Status.saveSettings!!.getString(Status.saveNameKey, "Гость")
-            if (Status.saveSettings!!.contains(Status.saveNeedKey))
-                Status.need = Status.saveSettings!!.getInt(Status.saveNeedKey, 7)
-            if (Status.saveSettings!!.contains(Status.saveLevelKey))
-                Status.level = Status.saveSettings!!.getInt(Status.saveLevelKey, 2)
-            if (Status.saveSettings!!.contains(Status.saveTimeModeKey))
-                Status.timeMode = Status.saveSettings!!.getBoolean(Status.saveTimeModeKey, false)
-            Log.d("tag123", "loaded")
-            Status.loaded = true
-        } catch (e : Exception){}
-
-        task.setText(Status.need.toString())
-
-//        task.addTextChangedListener(object : TextWatcher {
+//        number_tasks.addTextChangedListener(object : TextWatcher {
 //            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
 //
 //            }
@@ -91,7 +66,9 @@ class MainActivity : AppCompatActivity() {
 //                }
 //            }
 //        })
+    }
 
+    private fun setLevelsList() {
         val lvladap = ArrayAdapter(this, android.R.layout.simple_spinner_item, Status.levels)
         lvladap.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         lvl.adapter = lvladap
@@ -107,24 +84,56 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun update(){
-        userbutton.text = Status.userName
+    private fun loadDBdata() {
+        val db = dbHelper.writableDatabase
+
+        val cur = db.rawQuery("SELECT id FROM users;", null)
+        if (cur.moveToFirst())
+            do {
+                Status.existID.add(cur.getInt(cur.getColumnIndex("id")))
+            } while (cur.moveToNext())
+        cur.close()
+
+        if (!Status.loaded) try {
+            Status.saveSettings = getSharedPreferences(Status.nameFileSettings, Context.MODE_PRIVATE)
+
+            if (Status.saveSettings!!.contains(Status.saveIdKey))
+                Status.userID = Status.saveSettings!!.getInt(Status.saveIdKey, -1)
+            if (Status.saveSettings!!.contains(Status.saveNameKey))
+                Status.userName = Status.saveSettings!!.getString(Status.saveNameKey, "Гость")!!
+            if (Status.saveSettings!!.contains(Status.saveNeedKey))
+                Status.need = Status.saveSettings!!.getInt(Status.saveNeedKey, 7)
+            if (Status.saveSettings!!.contains(Status.saveLevelKey))
+                Status.level = Status.saveSettings!!.getInt(Status.saveLevelKey, 2)
+            if (Status.saveSettings!!.contains(Status.saveTimeModeKey))
+                Status.timeMode = Status.saveSettings!!.getBoolean(Status.saveTimeModeKey, false)
+            Log.d("tag123", "loaded")
+            Status.loaded = true
+        } catch (e : Exception){}
     }
 
-    override fun onResume() {
-        super.onResume()
+    private fun setButtonsListeners() {
+        userbutton.setOnClickListener {
+            val intent = Intent(this, UsersActivity::class.java)
 
-        update()
-    }
+            startActivity(intent)
+        }
 
-    override fun onStop() {
-        super.onStop()
+        settings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
 
-        try { Status.need = task.text.toString().toInt() } catch (e : Exception){}
+            startActivity(intent)
+        }
 
-        val editor = Status.saveSettings!!.edit()
-        editor.putInt(Status.saveLevelKey, Status.level)
-        editor.putInt(Status.saveNeedKey, Status.need)
-        editor.apply()
+        start.setOnClickListener {
+            try {
+                Status.need = number_tasks.text.toString().toInt()
+                val intent = Intent(this, Task::class.java)
+//                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                startActivity(intent)
+            } catch (e : Exception){
+                Toast.makeText(this, "Введите количество заданий", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
